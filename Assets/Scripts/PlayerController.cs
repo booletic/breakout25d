753ab534692e6c44fd54b2811bb71fa44f64ca74
Mansoor Nahar <h3rcul3z@gmail.com>
2022-07-03 03:51:27 +0400

    add limits to pedal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     public float horizontalInput;
+    public float boundry = 16.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +20,17 @@ public class PlayerController : MonoBehaviour
         // user-input to move pedal sideways
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.down);
+
+        // limit pedal right movement
+        if (transform.position.x > boundry)
+        {
+            transform.position = new Vector3(boundry, 1, 0);
+        }
+
+        // limit pedal left movement
+        if (transform.position.x < -boundry)
+        {
+            transform.position = new Vector3(-boundry, 1, 0);
+        }
     }
 }
