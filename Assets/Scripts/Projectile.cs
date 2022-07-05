8f67122cde82879d79ddbab5c3b4e3868b4cbf66
Mansoor Nahar <h3rcul3z@gmail.com>
2022-07-06 01:55:22 +0400

    destroy enemy on collision

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -17,4 +17,11 @@ public class Projectile : MonoBehaviour
     {
 
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,9 +8,11 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = -18; i < 18; i += 5)
+        for (int i = -18; i <= 18; i += 6)
         {
             Instantiate(enemyPrefab, new Vector3(i, 0.5f, 10), enemyPrefab.transform.rotation);
+            Instantiate(enemyPrefab, new Vector3(i, 0.5f, 7), enemyPrefab.transform.rotation);
+            Instantiate(enemyPrefab, new Vector3(i, 0.5f, 4), enemyPrefab.transform.rotation);
         }
     }
 
